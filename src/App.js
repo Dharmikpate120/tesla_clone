c703f79dfcc97edd98b40ca9f5e5b41c0e5f07a9
Dharmikpate120 <Dharmik7458@gmail.com>
2025-01-08 12:08:50 +0530

    initializing routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/*<Route path="/Signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/fillUserInfo" element={<FillUserInfo />} />
+
+        <Route path="/Profile" element={<ProfileNavbar />}>
+          <Route path="personalInformation" element={<ProfilePage />} />
+          <Route path="SellerInformation" element={<SellerProfile />} />
+          <Route path="previousorders" element={<PreviousOrder />} />
+          <Route path="Sellerform" element={<SellerForm />} />
+          <Route path="addNewProduct" element={<AddProducts />} />
+        </Route>
+        <Route path="/userDetails" element={<UserDetails />} />
+
+        <Route path="/Favourites" element={<LikedItems />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/confirmationPage" element={<ConfirmationPage />} />
+        <Route path="/paymentPage" element={<PaymentPage1 />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
